8c6d4182956bf3963d72db4570d63a982b17581b
stwer1998 <stwer1998@mail.ru>
2019-05-28 12:49:34 +0300

    fix

--- FileSharing/FileSharing/Controllers/FileController.cs
+++ FileSharing/FileSharing/Controllers/FileController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using FileSharing.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,12 +16,12 @@ namespace FileSharing.Controllers
     public class FileController : Controller
     {
         IHostingEnvironment _appEnvironment;
-        MyRepository repository;
+        MyDbContext db;
 
-        public FileController(IHostingEnvironment appEnvironment)
+        public FileController(MyDbContext context, IHostingEnvironment appEnvironment)
         {
             _appEnvironment = appEnvironment;
-            repository = new MyRepository();
+            db = context;
         }
         [HttpGet]
         public IActionResult ShareFile()
@@ -32,7 +35,7 @@ namespace FileSharing.Controllers
             var a = file.FileName.Split('.');
             var parh = _appEnvironment.WebRootPath + "/";// + key +
             string path = "." + a[a.Length - 1];
-            var key = repository.SaveSimpleFile(shortdescription, description, file.FileName, parh, path);
+            var key  = SaveSimpleFile(shortdescription, description, file.FileName, parh, path);
 
             if (file != null)
             {
@@ -52,14 +55,67 @@ namespace FileSharing.Controllers
 
         public FileResult GetFile(string url)
         {
-            var a = repository.GetFile(url);
+            var a = GetFile1(url);
             using (WebClient wc = new WebClient())
             {
                 var byteArr = wc.DownloadData(a.FilePath);
                 return File(byteArr, "multiple");
             }
         }
-            
+
+        private AplicationFile SaveSimpleFile(string shortdescription, string description, string filename, string path1, string path2)
+        {
+            string key = GetUniqueKey(6);
+            bool b = Cheker(key);
+            while (b)
+            {
+                key = GetUniqueKey(6);
+            }
+
+            AplicationFile file = new AplicationFile
+            {
+                NameFile = filename,
+                DiscriptionFile = description,
+                ShortDiscriptionFile = shortdescription,
+                FileCountDownload = 0,
+                Type = FileType.SimpleFile,
+                FileShortKey = key,
+                FilePath = path1 + key + path2
+            };
+            db.aplicationFiles.Add(file);
+            return file;
+        }
+
+        public AplicationFile GetFile1(string key)
+        {
+            //var b = db.aplicationFiles.All(x=>x);
+            var a = db.aplicationFiles.FirstOrDefault(x => x.FileShortKey == key);
+            return a;
+        }
+
+        private string GetUniqueKey(int size)
+        {
+            char[] chars =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[size];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+        private bool Cheker(string str)
+        {
+            var f = db.aplicationFiles.FirstOrDefault(x => x.FileShortKey == str);
+            if (f != null && f.FileShortKey != str) { return true; }
+            else return false;
+        }
         //[HttpGet]
         //public IActionResult GetFile(string url)
         //{
--- FileSharing/FileSharing/MyDbContext.cs
+++ FileSharing/FileSharing/MyDbContext.cs
@@ -9,19 +9,12 @@ namespace FileSharing
 {
     public class MyDbContext: DbContext
     {
+     
         public DbSet<AplicationFile> aplicationFiles { get; set; }
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
             Database.EnsureCreated();
-        }
-        const string connectionString = "localdb)\\mssqllocaldb;Database=EFProviders.InMemory;Trusted_Connection=True;MultipleActiveResultSets=true";
-        public MyDbContext()
-        {
-            Database.EnsureCreated();
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseInMemoryDatabase(connectionString);
-        }
+        }        
+       
     }
 }
